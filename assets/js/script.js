a4ef0818a90f372775998d6739f846a068050273
Andres Liu <andres.liu22@gmail.com>
2021-10-03 23:51:40 -0700

    Add comments to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,34 +5,44 @@ var apiKey = "cf55c07cb439558ab37bc0b13e34d8bd";
 var cityInfoEl = $('#city-info');
 var cityForecastEl = $('#city-forecast')
 
+// Handle every time the search button is pressed
 function handleFormSubmit(event){
     event.preventDefault();
+
+    // Delete spaces from input
     var cityName = inputEl.val().trim().replace(" ", "%20").toUpperCase();
 
+    // API URL to get the latitude and longitude of city
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
 
+    // Fetch data (latitude and longitude)
     fetch(apiUrl).then(function(response) {
         if (response.ok){
             response.json().then(function(data) {
-                // console.log(data.coord.lat + ", " + data.coord.lon);
+
+                // Set new API URL to get weather data from the typed city
                 var oneCallApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=" + apiKey;
+                // Fetch weather data of city
                 fetchWeatherData(oneCallApiUrl, data.name)
             })
         }
     });
 }
 
+// Fetch weather data of city
 function fetchWeatherData(url, city) {
     fetch(url).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
                 console.log(data);
+                // Display Information of city
                 showCurrentInfo(data, city);
             })
         }
     })
 }
 
+// Display current weather information of the city
 function showCurrentInfo(data, city) {
     cityInfoEl.empty();
     var h3 = $('<h3>');
@@ -72,6 +82,7 @@ function showCurrentInfo(data, city) {
     showForecast(data.daily);
 }
 
+// Display 5-day forecast of the city
 function showForecast(dailyData) {
     cityForecastEl.empty();
     var h3 = $('<h3>');
@@ -115,13 +126,15 @@ function showForecast(dailyData) {
     
 }
 
+// Change temperature from Kelvin to Fahrenheit
 function getTemperature(temp) {
     return ((temp - 270) * 9 / 5 + 32).toFixed(1);
 }
 
+// Return url of weather icon
 function getWeatherIcon(iconCode) {
-    console.log("https://openweathermap.org/img/wn/" + iconCode + "@2x.png")
     return "https://openweathermap.org/img/wn/" + iconCode + "@2x.png";
 }
 
+// Event listener
 submitBtnEl.on("click", handleFormSubmit);
